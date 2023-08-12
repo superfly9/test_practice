efbee72cfac92aa623abfd1dbb390b57b9c234fe
송승찬 <superfly09@daum.net>
2023-08-12 09:39:05 +0900

    fix: 불필요한 await 제거

--- src/components/Counter/Counter.test.js
+++ src/components/Counter/Counter.test.js
@@ -1,100 +1,98 @@
-import { render, screen } from "@testing-library/react"
-import Counter from "./Counter"
-import userEvent from '@testing-library/user-event'
-import { MAX_COUNT, MIN_COUNT } from "../../constant"
+import { render, screen } from "@testing-library/react";
+import Counter from "./Counter";
+import userEvent from "@testing-library/user-event";
+import { MAX_COUNT, MIN_COUNT } from "../../constant";
 
-const setup = () => render(<Counter />)
+const setup = () => render(<Counter />);
 
-describe('렌더링 테스트', ()=>{
-    test('첫 번째 렌더링시 화면 요소들을 체크한다.', ()=>{
-            setup();
-            expect(screen.getByText('Increase Count')).toBeInTheDocument();
-            expect(screen.getByText('Decrease Count')).toBeInTheDocument();
-            expect(screen.getByTestId('counter').textContent).toBe("0");
-        }
-    );
+describe("렌더링 테스트", () => {
+  test("첫 번째 렌더링시 화면 요소들을 체크한다.", () => {
+    setup();
+    expect(screen.getByText("Increase Count")).toBeInTheDocument();
+    expect(screen.getByText("Decrease Count")).toBeInTheDocument();
+    expect(screen.getByTestId("counter").textContent).toBe("0");
+  });
+});
 
-})
+describe("증가/감소 버튼의 동작을 체크한다.", () => {
+  test("증가 버튼 클릭시 카운트가 1씩 증가한다.", async () => {
+    setup();
+    const increaseBtn = await screen.findByText("Increase Count");
 
-describe('증가/감소 버튼의 동작을 체크한다.',()=>{
+    userEvent.click(increaseBtn);
+    userEvent.click(increaseBtn);
+    expect(screen.getByTestId("counter").textContent).toBe("2");
+  });
 
-    test('증가 버튼 클릭시 카운트가 1씩 증가한다.', async ()=>{
-        setup();
-        const increaseBtn = await screen.findByText('Increase Count');
+  test("감소 버튼 클릭시 카운트가 1씩 감소한다.", async () => {
+    setup();
+    const increaseBtn = await screen.findByText("Increase Count");
+    const decreaseBtn = await screen.findByText("Decrease Count");
 
-        await userEvent.click(increaseBtn);
-        await userEvent.click(increaseBtn);
-        expect(screen.getByTestId('counter').textContent).toBe("2")
-    })
+    userEvent.click(increaseBtn);
+    userEvent.click(increaseBtn);
+    userEvent.click(decreaseBtn);
+    expect(screen.getByTestId("counter").textContent).toBe("1");
+  });
 
-    test('감소 버튼 클릭시 카운트가 1씩 감소한다.', async ()=>{
-        setup();
-        const increaseBtn = await screen.findByText('Increase Count');
-        const decreaseBtn = await screen.findByText('Decrease Count');
+  test("count는 MAX_COUNT보다 커질 수 없다.", async () => {
+    setup();
+    const increaseBtn = await screen.findByText("Increase Count");
+    const CLICK_MORE_THAN_LIMIT = 5;
 
-        await userEvent.click(increaseBtn);
-        await userEvent.click(increaseBtn);
-        await userEvent.click(decreaseBtn);
-        expect(screen.getByTestId('counter').textContent).toBe("1")
-    })
+    for (let i = 0; i < MAX_COUNT + CLICK_MORE_THAN_LIMIT; i++) {
+      await userEvent.click(increaseBtn);
+    }
+    expect(Number(screen.getByTestId("counter").textContent)).toBe(MAX_COUNT);
+  });
 
-    test('count는 MAX_COUNT보다 커질 수 없다.', async ()=>{
-        setup();
-        const increaseBtn = await screen.findByText('Increase Count');
-        const CLICK_MORE_THAN_LIMIT = 5;
+  test("count는 MIN_COUNT보다 작아질 수 없다.", async () => {
+    setup();
+    const decreaseBtn = await screen.findByText("Decrease Count");
+    const CLICK_MORE_THAN_LIMIT = 5;
 
-        for (let i = 0; i < MAX_COUNT + CLICK_MORE_THAN_LIMIT; i++) {
-            await userEvent.click(increaseBtn);
-        }
-        expect(Number(screen.getByTestId('counter').textContent)).toBe(MAX_COUNT);
-    })
+    for (let i = 0; i < MAX_COUNT + CLICK_MORE_THAN_LIMIT; i++) {
+      userEvent.click(decreaseBtn);
+    }
+    expect(Number(screen.getByTestId("counter").textContent)).toBe(MIN_COUNT);
+  });
+});
 
-    test('count는 MIN_COUNT보다 작아질 수 없다.', async ()=>{
-        setup();
-        const decreaseBtn = await screen.findByText('Decrease Count');
-        const CLICK_MORE_THAN_LIMIT = 5;
+describe("counter의 변화량을 결정하는 amount가 존재한다.", () => {
+  test("counter의 변화량을 결정하는 amount의 기본 값은 1이다.", async () => {
+    setup();
+    const amount = screen.getByLabelText("클릭시 변화시킬 Count :", {
+      selector: "input",
+    });
+    expect(amount).toBeInTheDocument();
+    expect(amount).toHaveValue();
+    expect(amount.value).toBe("1");
+  });
 
-        for (let i = 0; i <  MAX_COUNT + CLICK_MORE_THAN_LIMIT; i++) {
-            await userEvent.click(decreaseBtn);
-        }
-        expect(Number(screen.getByTestId('counter').textContent)).toBe(MIN_COUNT);
-    })
-})
+  test("amount의 값을 5로 수정할 수 있다.", async () => {
+    setup();
+    const amount = screen.getByLabelText("클릭시 변화시킬 Count :"); // label의 for와 같은 id가진 input이 걸림
+    // This will search for the label that matches the given TextMatch, then find the element associated with that label.
+    expect(amount).toBeInTheDocument();
+    userEvent.type(amount, "{backspace}");
+    userEvent.type(amount, "5");
+    expect(amount.value).toBe("5");
+  });
 
+  test("amount의 값이 5일때 counter의 값이 5씩 변한다.", async () => {
+    setup();
+    const amount = screen.getByLabelText("클릭시 변화시킬 Count :", {
+      selector: "input",
+    });
+    userEvent.type(amount, "{backspace}");
+    userEvent.type(amount, "5");
+    expect(amount.value).toBe("5");
 
-describe('counter의 변화량을 결정하는 amount가 존재한다.', ()=>{
+    const increaseBtn = await screen.findByText("Increase Count");
+    userEvent.click(increaseBtn);
+    expect(screen.getByTestId("counter").textContent).toBe("5");
 
-    test('counter의 변화량을 결정하는 amount의 기본 값은 1이다.', async ()=>{
-        setup();
-        const amount = screen.getByLabelText('클릭시 변화시킬 Count :',{ selector : 'input' });
-        expect(amount).toBeInTheDocument();
-        expect(amount).toHaveValue();
-        expect(amount.value).toBe("1");
-    })
-
-    test('amount의 값을 5로 수정할 수 있다.', async ()=>{
-        setup();
-        const amount = screen.getByLabelText('클릭시 변화시킬 Count :'); // label의 for와 같은 id가진 input이 걸림
-        // This will search for the label that matches the given TextMatch, then find the element associated with that label.
-        expect(amount).toBeInTheDocument();
-        await userEvent.type(amount ,"{backspace}");
-        await userEvent.type(amount , "5");
-        expect(amount.value).toBe("5");
-    })
-
-    test('amount의 값이 5일때 counter의 값이 5씩 변한다.', async ()=>{
-        setup();
-        const amount = screen.getByLabelText('클릭시 변화시킬 Count :',{ selector :'input' });
-        await userEvent.type(amount ,"{backspace}");
-        await userEvent.type(amount , "5");
-        expect(amount.value).toBe("5");
-
-        const increaseBtn = await screen.findByText('Increase Count');
-        await userEvent.click(increaseBtn);
-        expect(screen.getByTestId('counter').textContent).toBe("5")
-
-        await userEvent.click(increaseBtn);
-        expect(screen.getByTestId('counter').textContent).toBe("10")
-        
-    })
-})
+    userEvent.click(increaseBtn);
+    expect(screen.getByTestId("counter").textContent).toBe("10");
+  });
+});
--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -4,76 +4,75 @@ import Login from "./Login";
 
 // https://jestjs.io/docs/mock-functions#mock-property
 const mockUsedNavigate = jest.fn();
-jest.mock('react-router-dom', () => ({
-   ...jest.requireActual('react-router-dom'),
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
   useNavigate: () => mockUsedNavigate,
 }));
 
 const setup = () => render(<Login />);
 
 describe("로그인 페이지의 유저 이벤트를 확인할 수 있다.", () => {
-
-  describe('기본적인 정보 입력이 가능하다.', ()=>{
-
+  describe("기본적인 정보 입력이 가능하다.", () => {
     test("아이디 입력이 가능하다", async () => {
       setup();
       const idInput = screen.getByLabelText("아이디");
-      await userEvent.type(idInput, "superfly9");
+      userEvent.type(idInput, "superfly9");
       expect(idInput.value).toEqual("superfly9");
     });
-  
+
     test("비밀번호 입력이 가능하다", async () => {
       setup();
       const passwordInput = screen.getByLabelText("비밀번호");
-      await userEvent.type(passwordInput, "1234");
+      userEvent.type(passwordInput, "1234");
       expect(passwordInput.value).toEqual("1234");
     });
-  })
+  });
 
-  describe('정보 입력 후 버튼 클릭시 결과를 확인 할 수 있다.', ()=>{
-
+  describe("정보 입력 후 버튼 클릭시 결과를 확인 할 수 있다.", () => {
     test("로그인 버튼을 누르면 입력된 값을 서버로 전송한다 - 1", async () => {
       setup();
-  
+
       const idInput = screen.getByLabelText("아이디");
       const passwordInput = screen.getByLabelText("비밀번호");
-  
-      await userEvent.type(idInput, "superfly9");
-      await userEvent.type(passwordInput, "seoul1234");
-      await userEvent.click(screen.getByRole("button"));
-  
-      expect(await screen.findByText('로그인 성공')).toBeInTheDocument();
+
+      userEvent.type(idInput, "superfly9");
+      userEvent.type(passwordInput, "seoul1234");
+      userEvent.click(screen.getByRole("button"));
+
+      expect(await screen.findByText("로그인 성공")).toBeInTheDocument();
       expect(mockUsedNavigate).toHaveBeenCalledTimes(1);
-    })
-  
+    });
+
     test("아이디 유효성 체크를 통과하지 못하면 에러 메시지를 띄운다.", async () => {
       setup();
-  
+
       const idInput = screen.getByLabelText("아이디");
       const passwordInput = screen.getByLabelText("비밀번호");
-  
-      await userEvent.type(idInput, "superfly123456");
-      await userEvent.type(passwordInput, "seoul1234");
-      await userEvent.click(screen.getByRole("button"));
-  
-      expect(await screen.findByTestId('id-error-message')).toBeInTheDocument();
+
+      userEvent.type(idInput, "superfly123456");
+      userEvent.type(passwordInput, "seoul1234");
+      userEvent.click(screen.getByRole("button"));
+
+      expect(await screen.findByTestId("id-error-message")).toBeInTheDocument();
       // getBy는 요소가 없을때 throw하기에 queryBy 이용
-      expect(screen.queryByText('로그인 성공')).not.toBeInTheDocument();
-    })
-  
+      expect(screen.queryByText("로그인 성공")).not.toBeInTheDocument();
+    });
+
     test("비밀번호 유효성 체크를 통과하지 못하면 에러 메시지를 띄운다.", async () => {
       setup();
-  
+
       const idInput = screen.getByLabelText("아이디");
       const passwordInput = screen.getByLabelText("비밀번호");
-  
-      await userEvent.type(idInput, "superfly9");
-      await userEvent.type(passwordInput, "1");
-      await userEvent.click(screen.getByRole("button"));
-  
-      expect(await screen.findByTestId('password-error-message')).toBeInTheDocument();
+
+      userEvent.type(idInput, "superfly9");
+      userEvent.type(passwordInput, "1");
+      userEvent.click(screen.getByRole("button"));
+
+      expect(
+        await screen.findByTestId("password-error-message")
+      ).toBeInTheDocument();
       // getBy는 요소가 없을때 throw하기에 queryBy 이용
-      expect(screen.queryByText('로그인 성공')).not.toBeInTheDocument();
-    })
-  })
-})
+      expect(screen.queryByText("로그인 성공")).not.toBeInTheDocument();
+    });
+  });
+});
