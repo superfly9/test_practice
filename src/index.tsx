c1c4b9a76d4683caa6c6c057dbb055f3a38a52ed
송승찬 <superfly09@daum.net>
2023-08-13 14:16:42 +0900

    fix: ts로 변환 - 패키지 설치 - 파일 확장자 변경

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import RootRouter from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider } from "react-router-dom";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+const router = RootRouter();
+root.render(<RouterProvider router={router} />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
