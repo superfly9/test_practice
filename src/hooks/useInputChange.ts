d07be74c617b1ad52e607f584e375fc32eb2425d
송승찬 <superfly09@daum.net>
2023-08-22 23:57:41 +0900

    fix: debounceValue 훅에 추가

--- src/hooks/useInputChange.ts
+++ src/hooks/useInputChange.ts
@@ -1,15 +1,16 @@
 import { useState } from 'react'
+import useDebounce from './useDebounce';
 
-function useInputChange(initialValue:string) {
-const [value,setValue] = useState(initialValue || '');
-
-const handleReset = () => {
-  setValue(initialValue);
-}
+function useInputChange(initialValue:string,isDebounce?:boolean) {
+  const [value,setValue] = useState(initialValue || '');
+  const deboucedVal = useDebounce(value)
+  const handleReset = () => {
+    setValue(initialValue);
+  }
 
 const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
   const { target : {value} } = e;
-  setValue(value);
+  setValue(isDebounce ? deboucedVal : value);
 }
   return [value, handleChange, handleReset] as const
 }
